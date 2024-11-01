a71ff88b538209857a50bbc44b45c778fb76f88e
Scacchetti <luiscacchetti07@gmail.com>
2024-11-01 04:21:25 -0300

    Card do Estoque na Dashboard

--- MarketProject/Controls/StorageDashboardCard.axaml.cs
+++ MarketProject/Controls/StorageDashboardCard.axaml.cs
@@ -1,6 +1,9 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+using MarketProject.Models;
 
 namespace MarketProject.Controls;
 
@@ -9,5 +12,22 @@ public partial class StorageDashboardCard : UserControl
     public StorageDashboardCard()
     {
         InitializeComponent();
+        UpdateDashboardCard();
+        Database.ProductsList.CollectionChanged += (_, _) =>
+        {
+            UpdateDashboardCard();
+        };
+    }
+    private void UpdateDashboardCard()
+    {
+        Dispatcher.UIThread.Post(() =>
+        {
+            int counting = Database.ProductsList.Count;
+            double percentegeValue = counting * 100 / ProductsProgressBar.Maximum;
+            DashboardCardMainContent.Text = $"{counting} itens";
+            ProductsProgressBar.Value = counting;
+            ProgressBarPercentage.Content = $"{Math.Round(percentegeValue,0)}%"; 
+        }, DispatcherPriority.Background);
+        
     }
 }
